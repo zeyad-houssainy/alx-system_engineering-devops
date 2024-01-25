94a43cc06445a275845fb4e847b3cd8f46b2236b
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-25 22:17:39 +0200

    Update 1-user_limit.pp.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp.pp
@@ -1,6 +1,13 @@
-# Setting upper limit for the pinging requests
-exec { 'upperlimit' :
-  command =>  'sed -i "s/15/10000/g" /etc/default/nginx;service nginx restart',
-  path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  =>  'test -e /etc/default/nginx',
+# Enable the user holberton to login and open files without error.
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
