50c79ed00161cdf6139830c898549be6637776e8
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-02-16 14:31:42 +0300

    1

--- 0x04-loops_conditions_and_parsing/test.sh
+++ 0x04-loops_conditions_and_parsing/test.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+# #!/bin/bash
 echo "Hello, World!"
-for i in {1..10}; do
-    echo "$i"
-done
+# for i in {1..10}; do
+#     echo "$i"
+# done
+# echo "hello, $USER. I wish to list some files of yours"
+# echo "listing files in the current directory, $PWD"
